080323fc3968841bca80e40b69903c1451bbc256
gisselvanessa <gisselvanessaa@gmail.com>
2023-01-12 12:48:53 -0500

    firt commit

---
+++ src/main.js
@@ -0,0 +1,63 @@
+const contactame=document.querySelector("#contactame");
+const modal=document.querySelector("#modal");
+modal.style.display='none';
+
+function showForm(){
+      // modal.close();
+      console.log("click");
+      modal.style.display='';
+}
+
+contactame.addEventListener("click", function(){
+      showForm();
+})
+
+function showAlert(){
+      const formRegister = document.querySelector("#formRegister");
+
+      formRegister.addEventListener("submit", function (e) {
+      e.preventDefault();
+
+      const nameUser = e.target.user_name.value.trim();
+      const mailUser = e.target.user_mail.value.trim();
+      const messageUser = e.target.user_message.value.trim();
+
+      if (
+            nameUser === "" ||
+            mailUser === "" ||
+            messageUser===""
+            
+      ) {
+            alert("Es necesario llenar todos los campos");
+      }
+      else{
+            alert("Se ha enviado correctamente");
+      }
+})
+}
+
+
+
+function showMenu() {
+      const iconMenu = document.querySelector(".icon__menu");
+      const menu = document.querySelector(".menu");
+      
+      iconMenu.addEventListener("click", function () {
+      console.log("click hamburguer");
+      menu.classList.toggle("menu__show");
+      });
+
+}
+
+function closeMenu(){
+      const menu = document.querySelector(".menu");
+      menu.addEventListener("click", function(){
+      
+      menu.classList.toggle("menu__show");
+      } )
+
+}
+showAlert();
+showMenu();
+closeMenu();
+
